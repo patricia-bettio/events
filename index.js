199c8492144e7e765c924b3b89e0595f353381ff
pfbettio <pfbettio@gmail.com>
2019-11-05 23:52:47 +0100

    fetch of: price, title and subpage with long description

---
+++ index.js
@@ -0,0 +1,60 @@
+/* window.addEventListener("DOMContentLoaded", getData);
+
+function getData(){
+    console.log("getData")
+    fetch("http://dredesigns.dk/MyWordpress/wp-json/wp/v2/")
+    .then(res=>res.json())
+    .then(useData)
+}
+
+function useData(myData){
+    console.log(myData)
+}*/
+
+window.addEventListener("DOMContentLoaded", getData);
+
+function getData(){
+    //console.log("getData")
+    fetch("http://dredesigns.dk/MyWordpress/wp-json/wp/v2/concerts_theatre_eve/")
+    .then(res=>res.json())
+    .then(useData)
+}
+
+function useData(myData){
+    //console.log(myData)
+
+    //1- Loop the array
+    myData.forEach(showEvent)
+}
+
+function showEvent(event){
+   console.log(event)
+    //2- Clone the template
+    //image
+    /*const imgPath = event._embedded["wp:featuredmedia"][0].media_details.sizes.thumbnail.source_url;*/
+
+    const template = document.querySelector(".eventTemplate").content;
+    const eventCopy = template.cloneNode(true);
+    //3- textcontent and ineer HTML
+    const h1 = eventCopy.querySelector("h1");
+    h1.textContent = event.title.rendered;
+
+    const prices = eventCopy.querySelector(".prices");
+    prices.innerHTML = event.price;
+
+
+
+    /*image
+    const img = eventCopy.querySelector("img.cover");
+
+    img.setAttribute("src", imgPath)
+    img.setAttribute("alt", "Cover of the book" + event.title.rendered)*/
+
+    //subpage
+    const a = eventCopy.querySelector("a");
+    a.href="sub.html?id="+event.id
+
+    //4- Append
+    document.querySelector("#events").appendChild(eventCopy);
+
+}
---
+++ sub.js
@@ -0,0 +1,16 @@
+const urlParams = new URLSearchParams(window.location.search);
+const id = urlParams.get("id");
+console.log(id)
+
+
+fetch("http://dredesigns.dk/MyWordpress/wp-json/wp/v2/concerts_theatre_eve/"+id)
+.then(res=>res.json())
+.then(showEvent)
+
+function showEvent(event){
+    console.log(event)
+    document.querySelector("article h1").textContent=event.title.rendered;
+
+    document.querySelector(".longdescription").textContent=event.content.rendered;
+
+}
